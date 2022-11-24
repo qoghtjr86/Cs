2007b4eb77ef9092b62bd2775e7d25a17c769859
Bae Hoseog <qoghtjr86@gmail.com>
2022-11-24 20:09:44 +0900

    Commit 2

---
+++ Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Vector3.down;
+
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+
+        Destroy(gameObject);
+    }
+}
