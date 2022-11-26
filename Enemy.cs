40166999da9c3e4c2286ea39dbc9f910ae67820d
Bae Hoseog <qoghtjr86@gmail.com>
2022-11-26 19:59:26 +0900

    Commit 2

--- Enemy.cs
+++ Enemy.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 5;
-
+    Vector3 dir;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        int randValue = UnityEngine.Random.Range(0, 10);
+
+        if(randValue < 3)
+        {
+            GameObject target = GameObject.Find("Player");
+            dir = target.transform.position - transform.position;
+            dir.Normalize();
+        }
+
+        else
+        {
+            dir = Vector3.down;
+        }
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Vector3 dir = Vector3.down;
-
+    { 
         transform.position += dir * speed * Time.deltaTime;
     }
 
